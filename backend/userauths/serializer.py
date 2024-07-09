67e93c8284c5581e3fc8a079508d9b35cdacc1d2
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-09 13:35:04 +0300

    17- Login Serializer using JWT

--- backend/userauths/serializer.py
+++ backend/userauths/serializer.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token
 from userauths.models import Profile, User
 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+  @classmethod
+  def get_token(cls, user):
+    token = super().get_token(user)
+
+    token['full_name'] = user.full_name
+    token['email'] = user.email
+    token['username'] = user.username
+    try:
+      token['vendor_id'] = user.vendor.id
+    except:
+      token['vendor_id'] = 0
+
+    return token
 
 class UserSerializer(serializers.ModelSerializer):
   
